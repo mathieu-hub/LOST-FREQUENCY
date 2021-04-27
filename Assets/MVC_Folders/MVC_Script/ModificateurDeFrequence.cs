502b380a11c36607bf011ca80cc581f43387bfea
mathieu-hub <m.vancappellen@rubika-edu.com>
2021-04-27 23:23:53 +0200

    Interruptor Works

---
+++ Assets/MVC_Folders/MVC_Script/ModificateurDeFrequence.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModificateurDeFrequence : MonoBehaviour
+{
+    public static ModificateurDeFrequence Instance;
+
+    [Header("ON / OFF")]
+    public bool deviceIsOn = false;
+    public GameObject interruptorOnOff;
+    public Animator animatorOnOff;
+
+    [Header("VOYAGE TRANSFREQUENTIEL")]
+    public bool activeVoyageTf = false;
+    public GameObject interruptorVoyageTf;
+    //public Animator animatorVoyageTf;
+    
+
+    [Header("ANOMALIE DETECTEUR")]
+    public List<GameObject> anomalieLeds;
+
+    [Header("MATERIALS REFERENCES")]
+    public List<Material> materials;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (deviceIsOn)
+        {
+            animatorOnOff.SetBool("switchOn", true);
+        }
+        else if (!deviceIsOn)
+        {
+            animatorOnOff.SetBool("switchOn", false);
+        }
+
+    }
+}
