212be3b82e8c68a9b0c7f4e229b61179d8c545ec
mathieu-hub <m.vancappellen@rubika-edu.com>
2021-05-14 23:47:49 +0200

    Push Before Testing NavMesh on Alien

---
+++ Assets/MVC_Folders/MVC_Script/AlienAI.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AlienAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States 
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    void Awake()
+    {
+        player = GameObject.Find("HEROPLAYER").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //WalkPoint Reached
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false; 
+        }
+    }
+
+    void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    void AttackPlayer()
+    {
+        //Make sure Alien doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            ///Attack Code Here
+            Debug.Log("Alien Attack");
+            ///
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+}
